0f28a32028140e640d189298ddcac45dc0c40218
shivvamm <shivam574488@gmail.com>
2023-09-05 16:06:04 +0530

    Processing CLI arg

--- index.js
+++ index.js
@@ -3,4 +3,9 @@
 
  // A hashbang is a special comment placed at the top of a script that tells the operating system which interpreter to use when running the script. In the context of a Node.js CLI app, the hashbang tells the machine to use Node.js as the interpreter to execute the JS file. 
 
-console.log('hello')
+const note = process.argv[2];
+const newNote ={
+content : note,
+id:Date.now()
+}
+console.log(newNote);
