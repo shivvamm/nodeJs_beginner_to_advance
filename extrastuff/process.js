73a72f7bbb171f2ed121785c5a6a19ffa3a0e209
shivvamm <shivam574488@gmail.com>
2023-09-05 15:59:52 +0530

    process

---
+++ extrastuff/process.js
@@ -0,0 +1,6 @@
+// Process and Environment
+console.log(process); // globals object provides info about the Nodejs process ands allows to interact with it
+console.log(process.argv) // array of command line arguments
+console.log(process.pid)  //  current process ID
+console.log(process.env) //environment varibale obj of crr process
+console.log(process.exit)  //terminate current processs
